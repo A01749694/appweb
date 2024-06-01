ec4ceac00eb5d3e5253b61fdac6ff44b946e0752
Seb <117104224+A01749694@users.noreply.github.com>
2024-06-01 15:46:14 +0000

    Version 1.0.2

--- appweb/src/components/Calendario.js
+++ appweb/src/components/Calendario.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import 'devextreme/dist/css/dx.light.css';
 import { Calendar } from 'devextreme-react/calendar';
+import SelectBox from 'devextreme-react/select-box';
 
-
+const zoomLevels = ['month', 'year', 'decade', 'century'];
+const zoomLevelLabel = { 'aria-label': 'Zoom Level' };
 
 const Calendario = () => {
+    const [zoomLevel, setZoomLevel] = useState('month');
+
+    const onZoomLevelChange = useCallback(({ value }) => {
+        setZoomLevel(value);
+    }, []);
+
     return (
-        <div>
-            <Calendar>
-            </Calendar>
+        <div className="w3-container">
+            <h1 className="w3-center">Calendario</h1>
+            <div className="w3-margin-bottom">
+                <Calendar
+                    zoomLevel={zoomLevel}
+                    showTodayButton={true}
+                />
+            </div>
+            <div className="w3-margin-top">
+                <span>Zoom level: </span>
+                <SelectBox
+                    dataSource={zoomLevels}
+                    value={zoomLevel}
+                    inputAttr={zoomLevelLabel}
+                    onValueChanged={onZoomLevelChange}
+                />
+            </div>
         </div>
     );
 };
